f1c62dbf454a63cf414e2581faa809d6e0c3999f
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-08-15 19:01:27 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,21 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   pageLoadTimeout: 60000,
   chromeWebSecurity: false,
-  viewportHeight: 1080,
   viewportWidth: 1920,
+  viewportHeight: 1080,
   e2e: {
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+          const width = 2400
+          const height = 1500
+          console.log('setting the browser window size to', width, height);
+          if (browser.name === 'chrome' && 'electron') {
+            launchOptions.args.push(`--window-size=${width},${height}`)
+            launchOptions.args.push('--force-device-scale-factor=1')
+          };
+          return launchOptions;
+        })
+    },
     baseUrl: 'https://hh.ru',
     projectId: "mhpy6x",
     defaultCommandTimeout: 100000,
