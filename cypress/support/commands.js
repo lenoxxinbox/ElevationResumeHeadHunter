4627d2993d7c5d47d6517d7d3c90bee52ad55d8c
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-08-16 10:59:57 +0000

    Upd

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,7 @@ import element from '../fixtures/elements.json'
 Cypress.Commands.add('login', (email, password) => {    
     cy.get(element.invite).click();
     cy.get(element.toPass).click();
-    cy.get(element.email).type(email).should('have.value', email);
+    cy.get(element.email).scrollIntoView().type(email).should('have.value', email);
     cy.get(element.pass).type(password);
     cy.get(element.logInButton).click();
 })
