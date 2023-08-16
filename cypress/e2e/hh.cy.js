7933be9c08b20c608cec793105db80ad53db14d4
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-08-16 13:54:28 +0300

    Update hh.cy.js

--- cypress/e2e/hh.cy.js
+++ cypress/e2e/hh.cy.js
@@ -15,9 +15,9 @@ describe('MyResume', () => {
     cy.login(email, password);
     cy.scrollTo('top');
     cy.get(element.myResumeMenu).should('be.visible').click();
-    cy.contains(element.raiseInSearchText).should('be.visible').click();
-    cy.get(element.modalWindow).should('be.visible');
-    cy.contains(element.ok).should('be.visible').click();
-    cy.get(element.finalText).scrollIntoView().should('be.visible');
+    cy.contains(element.raiseInSearchText).should('be.visible');
+    // cy.get(element.modalWindow).should('be.visible');
+    // cy.contains(element.ok).should('be.visible').click();
+    // cy.get(element.finalText).scrollIntoView().should('be.visible');
   })
 })
