91f0e6231c886eba97a5e8ae1af0c260f3ae61b8
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-08-16 13:53:41 +0300

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,13 +2,8 @@ import element from '../fixtures/elements.json'
 
 Cypress.Commands.add('login', (email, password) => {    
     cy.get(element.invite).click();
-    cy.get(element.toPass).click().then(() => {
-        if(email) {
-            cy.get(element.email).type(email).should('have.value', email);
-        }
-        if(password) {
-            cy.get(element.pass).type(password);
-        }
-    });
+    cy.get(element.toPass).click();
+    cy.get(element.email).type(email).should('have.value', email);
+    cy.get(element.pass).type(password);
     cy.get(element.logInButton).click();
 })
