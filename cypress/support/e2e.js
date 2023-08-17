e161e540781a75eae1c12bb77b6d45988385b3a6
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-08-17 15:02:38 +0300

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -6,18 +6,34 @@ Cypress.on('uncaught:exception', (err, runnable) => {
     }
 });
 
+Cypress.on('window:before:load', function (window) {
+    const original = window.EventTarget.prototype.addEventListener
+  
+    window.EventTarget.prototype.addEventListener = function () {
+      if (arguments && arguments[0] === 'beforeunload') {
+        return
+      }
+      return original.apply(this, arguments)
+    }
+  
+    Object.defineProperty(window, 'onbeforeunload', {
+      get: function () { },
+      set: function () { }
+    })
+  });
+  
 if (Cypress.config('hideXHRInCommandLog')) {
-  const app = window.top;
-
-  if (
-    app &&
-    !app.document.head.querySelector('[data-hide-command-log-request]')
-  ) {
-    const style = app.document.createElement('style');
-    style.innerHTML =
-      '.command-name-request, .command-name-xhr { display: none }';
-    style.setAttribute('data-hide-command-log-request', '');
-
-    app.document.head.appendChild(style);
+    const app = window.top;
+  
+    if (
+      app &&
+      !app.document.head.querySelector('[data-hide-command-log-request]')
+    ) {
+      const style = app.document.createElement('style');
+      style.innerHTML =
+        '.command-name-request, .command-name-xhr { display: none }';
+      style.setAttribute('data-hide-command-log-request', '');
+  
+      app.document.head.appendChild(style);
+    }
   }
-}
