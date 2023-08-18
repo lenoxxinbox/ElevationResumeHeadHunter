bb29f8b049cbce600ebece52d9473e17ba270a4b
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-08-18 09:31:07 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  pageLoadTimeout: 120000,
+  pageLoadTimeout: 600000,
   chromeWebSecurity: false,
   viewportWidth: 1920,
   viewportHeight: 1080,
